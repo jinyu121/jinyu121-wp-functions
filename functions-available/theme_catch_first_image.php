8b7dc893ce678fe80ced95f98e971ac0a9c119e5
Yu Hao <jinyu121@gmail.com>
2017-08-27 20:44:37 +0800

    修改了获取第一章图片的函数：优先从摘要里面提取第一张图片

--- functions-available/theme_catch_first_image.php
+++ functions-available/theme_catch_first_image.php
@@ -10,15 +10,34 @@
 // |    抓取文章的第一个图片当做缩略图
 // +----------------------------------------------------------------------+
 
-function theme_catch_first_image($default=false) {
-    global $post, $posts;
-    ob_start();
-    ob_end_clean();
-    $doc = new DOMDocument();
-    @$doc->loadHTML($post->post_content);
-    $img = $doc->getElementsByTagName('img');
-    if($img->length > 0){
-        return $img->item(0)->getAttribute('src');
+if (!function_exists("theme_catch_first_image")) {
+    function __jinyu121_first_image($html)
+    {
+        if (empty($html)) {
+            return false;
+        }
+        $doc = new DOMDocument();
+        @$doc->loadHTML($html);
+        $img = $doc->getElementsByTagName('img');
+        if ($img->length > 0) {
+            return $img->item(0)->getAttribute('src');
+        }
+        return false;
     }
-    return $default;
+
+    function theme_catch_first_image($default=false)
+    {
+        global $post;
+        ob_start();
+        ob_end_clean();
+        $img = __jinyu121_first_image($post->post_excerpt);
+        if (empty($img)) {
+            $img = __jinyu121_first_image($post->post_content);
+            if (empty($img)) {
+                $img = $default;
+            }
+        }
+
+        return $img;
+    }
 }
